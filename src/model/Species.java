cdb2fc1ade10dfbc59eb21c15c35987749390537
XxcassianxX <dannastul@gmail.com>
2024-11-10 13:43:04 -0500

    Entregar trabajo

--- src/model/Species.java
+++ src/model/Species.java
@@ -1,31 +1,29 @@
 package model;
 
-public class Species {
+abstract class Specie {
+	protected String name;
+	protected String scientificName;
 
-	private String name;
-	private String scientificName;
-
-	public Species(String name, String scientificName) {
-
+	public Specie(String name, String scientificName) {
 		this.name = name;
 		this.scientificName = scientificName;
-
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	public String getCientificName() {
+		return scientificName;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getScientificName() {
-		return scientificName;
+	public void setCientificName(String cientificName) {
+		this.scientificName = cientificName;
 	}
 
-	public void setScientificName(String scientificName) {
-		this.scientificName = scientificName;
-	}
-
+	public abstract void showDetail(); // Método abstracto para mostrar detalles específicos
 }
